f2f0a6ed6f19850e8fcaa3e6a3f9a093a0826a37
C W <wahlers1@gmail.com>
2017-04-15 09:56:28 -0400

    New mlab account

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ app.use(express.static("public"));
 
 // Database configuration with mongoose
 //mongoose.connect("mongodb://localhost/hw18scrape");
-mongoose.connect("mongodb://heroku_sf716p4j:3tnrvfduaadugdb7eefb6760a6@ds161580.mlab.com:61580/heroku_sf716p4j")
+//mongoose.connect("mongodb://heroku_sf716p4j:3tnrvfduaadugdb7eefb6760a6@ds161580.mlab.com:61580/heroku_sf716p4j");
+mongoose.connect("mongodb://cwahlers:KyleCole1@ds161580.mlab.com:61580/heroku_sf716p4j");
+
 var db = mongoose.connection;
 
 // Show any mongoose errors
